8bb7e07410f4a990bdc37e1c0f4829cdb35f665c
Katy DeCorah <katydecorah@gmail.com>
2022-01-19 10:08:49 -0500

    Fix tests

--- src/__tests__/add-game.test.ts
+++ src/__tests__/add-game.test.ts
@@ -32,42 +32,32 @@ const sample = {
 
 describe("addGame", () => {
   test("works", async () => {
-    expect(await addGame({ ...sample, fileName: "my-wordle.yml" }))
-      .toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "board": Array [
-            "🟩⬛⬛⬛⬛",
-            "⬛⬛🟨🟩🟨",
-            "🟩🟩🟩🟩🟩",
-          ],
-          "boardWords": Array [
-            "yes no no no no",
-            "no no almost yes almost",
-            "yes yes yes yes yes",
-          ],
-          "date": "2022-01-17",
-          "number": 209,
-          "score": 3,
-          "won": true,
-        },
-        Object {
-          "board": Array [
-            "🟩⬛⬛⬛⬛",
-            "⬛⬛🟨🟩🟨",
-            "🟩🟩🟩🟩🟩",
-          ],
-          "boardWords": Array [
-            "yes no no no no",
-            "no no almost yes almost",
-            "yes yes yes yes yes",
-          ],
-          "date": "2022-01-18",
-          "number": 210,
-          "score": 3,
-          "won": true,
-        },
-      ]
-    `);
+    jest.useFakeTimers().setSystemTime(new Date("2022-01-18").getTime());
+    expect(await addGame({ ...sample, fileName: "my-wordle.yml" })).toEqual([
+      {
+        board: ["🟩⬛⬛⬛⬛", "⬛⬛🟨🟩🟨", "🟩🟩🟩🟩🟩"],
+        boardWords: [
+          "yes no no no no",
+          "no no almost yes almost",
+          "yes yes yes yes yes",
+        ],
+        date: "2022-01-17",
+        number: 209,
+        score: 3,
+        won: true,
+      },
+      {
+        board: ["🟩⬛⬛⬛⬛", "⬛⬛🟨🟩🟨", "🟩🟩🟩🟩🟩"],
+        boardWords: [
+          "yes no no no no",
+          "no no almost yes almost",
+          "yes yes yes yes yes",
+        ],
+        date: "2022-01-18",
+        number: 210,
+        score: 3,
+        won: true,
+      },
+    ]);
   });
 });
