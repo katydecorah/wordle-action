93afc6c303a7c35551b2e08638831b09f4ba3e33
Katy DeCorah <katydecorah@gmail.com>
2022-02-05 10:17:58 -0500

    Fix bug

--- src/to-json.ts
+++ src/to-json.ts
@@ -4,7 +4,7 @@ import { load } from "js-yaml";
 export default async function toJson(fileName: string) {
   try {
     const contents = (await returnReadFile(fileName)) as string;
-    return load(contents);
+    return contents ? load(contents) : [];
   } catch (error) {
     throw new Error(error);
   }
