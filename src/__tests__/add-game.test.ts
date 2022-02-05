4ac8992082b549f2a5d09bcbe73d8d0ca35f4613
Katy DeCorah <katydecorah@gmail.com>
2022-02-05 10:17:34 -0500

    Prove bug: `TypeError: Cannot read properties of undefined (reading 'push')`

--- src/__tests__/add-game.test.ts
+++ src/__tests__/add-game.test.ts
@@ -2,7 +2,7 @@ import addGame from "../add-game";
 
 jest.mock("@actions/core");
 
-const mockReadFile = Promise.resolve(
+let mockReadFile = Promise.resolve(
   JSON.stringify([
     {
       board: ["🟩⬛⬛⬛⬛", "⬛⬛🟨🟩🟨", "🟩🟩🟩🟩🟩"],
@@ -52,6 +52,25 @@ describe("addGame", () => {
         score: 3,
         won: true,
       },
+      {
+        board: ["🟩⬛⬛⬛⬛", "⬛⬛🟨🟩🟨", "🟩🟩🟩🟩🟩"],
+        boardWords: [
+          "yes no no no no",
+          "no no almost yes almost",
+          "yes yes yes yes yes",
+        ],
+        date: "2022-01-18",
+        number: 210,
+        score: 3,
+        won: true,
+      },
+    ]);
+  });
+
+  test("can add wordle game to empty yaml file", async () => {
+    jest.useFakeTimers().setSystemTime(new Date("2022-01-18").getTime());
+    mockReadFile = Promise.resolve("");
+    expect(await addGame({ ...sample, fileName: "my-wordle.yml" })).toEqual([
       {
         board: ["🟩⬛⬛⬛⬛", "⬛⬛🟨🟩🟨", "🟩🟩🟩🟩🟩"],
         boardWords: [
